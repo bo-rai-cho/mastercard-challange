115f01b74fb949ebd0850bb7d6c2b146cc23a296
Nikolay Ponomarev <nikolay.a.ponomarev@gmail.com>
2018-02-27 16:29:38 -0800

    Added one more test - check not english word

--- words-counter/src/test/java/WordsCounterExceptionTest.java
+++ words-counter/src/test/java/WordsCounterExceptionTest.java
@@ -30,6 +30,7 @@ public class WordsCounterExceptionTest {
                  "Try this..." , // Plural dots in the end -> not valid
                  "       What       about        this?          " , // Spaces in the beginning and in the end
                  "Try this;" , // Dot and comma in the end
+                "Не английское словечко." , // Not english sentence
                  "Try this???" , // Plural '?' characters
                  "Ho891723, 7kjh*&@!%#b" , // What is that?
                  "." , // Just comma
